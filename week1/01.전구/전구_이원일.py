a6d10430eece2e720f979d8586fb9408b50a1ff0
illizard <57248121+illizard@users.noreply.github.com>
2024-03-18 22:33:04 +0900

    Update 전구_이원일.py
    still 틀림

--- week1/01.전구/전구_이원일.py
+++ week1/01.전구/전구_이원일.py
@@ -1,8 +1,9 @@
 # # 문제 이해 - 요구사항 정리 - 패턴 분석 - 구현 - 예외 케이스
+# 중요 포인트: 1. 문제에서 나오는 전구 리스트의 인덱스와 실제 리스트의 인덱스 차이
 
 import sys
 num_bulb, num_command = map(int, sys.stdin.readline().split()) 
-bulb_list = list(map(int, sys.stdin.readline().split())) 
+bulb_list = [0] + list(map(int, sys.stdin.readline().split())) 
 # command_list = list(map(int, sys.stdin.readline().split()))
 
 command_mat = [list(map(int, sys.stdin.readline().split())) for _ in range(num_command)]
@@ -17,31 +18,53 @@ def cmd_1(bulb_list, command_list):
     
     return bulb_list
 
-# # 2번째 명령 실행 함수
-# def cmd_2(bulb_list, command_idx):
-#     pass
+# 2번째 명령 실행 함수 # 2,4 일떄
+def cmd_2(bulb_list, command_list):
+    l = command_list[1] # l = 1
+    r = command_list[2] # r = 3
+    for i in range(l, r+1): # i = 1, 2, 3
+        if bulb_list[i] == 0:
+            bulb_list[i] += 1
+        else:
+            bulb_list[i] -= 1
 
-# # 3번째 명령 실행 함수
-# def cmd_3(bulb_list, command_idx):
-#     pass
+    return bulb_list
 
-# # 4번째 명령 실행 함수
-# def cmd_4(bulb_list, command_idx):
-#     pass    
+# 3번째 명령 실행 함수
+def cmd_3(bulb_list, command_list):
+    l = command_list[1]
+    r = command_list[2]
+
+    #bulb_list[l:r] = 0 -> 이런 문법 없음
+    # 범위 l~r을 0으로 채운 리스트로 교체
+    bulb_list[l:r+1] = [0] * (r+1 - l)
+
+    return bulb_list
+
+# 4번째 명령 실행 함수
+def cmd_4(bulb_list, command_list):
+    l = command_list[1]
+    r = command_list[2]
+    bulb_list[l:r+1] = [1] * (r+1 - l)
+
+    return bulb_list
+    
 
 def main(num_bulb, num_command, bulb_list, command_mat):
     # for command_list in command_mat:
     for command_list in command_mat:
+        # print(command_list)
         # 1번째 명령
         if command_list[0] == 1:
             bulb_list = cmd_1(bulb_list, command_list)
         elif command_list[0] == 2:
-            pass    
+            bulb_list = cmd_2(bulb_list, command_list)
         elif command_list[0] == 3:
-            pass
+            bulb_list = cmd_3(bulb_list, command_list)
         else:
-            pass
-    # print(command_mat)
-    return bulb_list
+            bulb_list = cmd_4(bulb_list, command_list)
+
+    print(bulb_list[1:])
+    return bulb_list[1:]
 
 main(num_bulb, num_command, bulb_list, command_mat)
