06c79b01c79f119cae92ac0397a15a9737de22d2
illizard <57248121+illizard@users.noreply.github.com>
2024-03-22 14:05:41 +0900

    Update 전구_이원일.py

--- week1/01.전구/전구_이원일.py
+++ week1/01.전구/전구_이원일.py
@@ -72,29 +72,41 @@
 # main(num_bulb, num_command, bulb_list, command_mat)
 # #####################################################################
 
+# 
+# elif a == 4:
+#         s[b:c+1] = 1
+#TypeError: can only assign an iterable
+
 ### solution 2
-n, m = map(int,input().split())
-bulb = [0] + list(map(int,input().split()))
-for _ in range(m):
-	a,b,c = map(int, input().split())
-	if a == 1:
-		bulb[b] = c
-	
-	elif a == 2:  # 전구 상태 변경
-		for i in range(b,c+1):
-			if bulb[i] == 0:
-				bulb[i] = 1
-			elif bulb[i] == 1:
-				bulb[i] = 0
+    
+import sys
+input = sys.stdin.readline
 
-	elif a == 3:  # 켜진 전구 끄기
-		for i in range(b,c+1):
-			if bulb[i] == 1:
-				bulb[i] = 0
-	elif a == 4:  # 꺼진 전구 켜기
-		for i in range(b,c+1):
-			if bulb[i] == 0:
-				bulb[i] = 1
+N, M= map(int, input().strip().split())
 
-for i in range(1,n+1):
-	print(bulb[i], end=' ')
+s = [0] + list(map(int, input().strip().split()))
+
+for idx in range(M):
+    a, b, c = map(int, input().strip().split())
+    if a == 1:
+        s[b] = c
+    elif a == 2:
+        for idx in range(b, c+1):
+            if s[idx] == 0:
+                s[idx] = 1
+            else:
+                s[idx] = 0
+    elif a == 3:
+        for idx in range(b, c+1):
+            if s[idx] == 1:
+                s[idx] = 0
+            else:
+                pass
+    elif a == 4:
+        for idx in range(b, c+1):
+            if s[idx] == 0:
+                s[idx] = 1
+            else:
+                pass 
+for _ in range(1, len(s)):
+    print(s[_], end=' ')
