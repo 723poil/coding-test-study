6e2ac45aaf25bbb76fe12a19f6266a3f5f2264b1
김현지 <86637372+aacara@users.noreply.github.com>
2024-04-02 20:49:12 +0900

    김현지 3주차 2문제 (#18)
    * 2주차_04BFS와DFD_김현지
    
    * 2주차_05유기농 배추_김현지
    
    * 2주차_06음식물 피하기_김현지
    
    * 2주차_07미로탐색_김현지
    
    * 2주차_08토마토_김현지
    
    * 2주차_09 벽 부수고 이동하기_김현지
    
    * 2주차_01 피보나치 수_김현지
    
    * 2주차_02 피보나치 수 2_김현지

---
+++ week3/01. 피보나치 수 5/피보나치_수_5_김현지.py
@@ -0,0 +1,13 @@
+def solution():
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+n = int(input())
+dp = [0] * (n+1)
+print(solution())
