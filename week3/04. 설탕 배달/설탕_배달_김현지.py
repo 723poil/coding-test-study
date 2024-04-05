2d9cf4a09e79fb1c06e455a80a93a63a70a2c306
김현지 <86637372+aacara@users.noreply.github.com>
2024-04-05 12:53:47 +0900

    3주차 3문제 (#21)
    * 2주차_04BFS와DFD_김현지
    
    * 2주차_05유기농 배추_김현지
    
    * 2주차_06음식물 피하기_김현지
    
    * 2주차_07미로탐색_김현지
    
    * 2주차_08토마토_김현지
    
    * 2주차_09 벽 부수고 이동하기_김현지
    
    * 3주차_04설탕 배달_김현지
    
    * 3주차_05 1로 만들기_김현지
    
    * 3주차_03돌 게임_김현지

---
+++ week3/04. 설탕 배달/설탕_배달_김현지.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+# 상근이가 배달하는 봉지의 최소 개수, 정확하게 N 킬로그램을 만들 수 없다면 -1 출력
+dp = [0] * (N+1)
+
+# 초기화
+dp[3] = 1
+if N >= 5:
+    dp[5] = 1
+
+for i in range(4, N+1):
+    minCnt = min(dp[i-3], dp[i-5])
+    if dp[i-3] == 0 and dp[i-5] == 0:
+        continue
+    elif dp[i-3] != 0 and dp[i-5] != 0:
+        dp[i] = min(dp[i-3], dp[i-5]) + 1
+    elif dp[i-3] == 0 and dp[i-5] != 0:
+        dp[i] = dp[i-5] + 1
+    elif dp[i-3] != 0 and dp[i-5] == 0:
+        dp[i] = dp[i-3] + 1
+
+if dp[N] != 0:
+    print(dp[N])
+else:
+    print(-1)
