92ebe7dccfc18aae7ddcf714ee9ca19c1276cfbe
gam_bit91 <gam_bit91@naver.com>
2024-05-03 00:10:26 +0900

    week7 04 improved (3/6)

--- week7/04.부분수열의 합/부분수열의_합_강민지.py
+++ week7/04.부분수열의 합/부분수열의_합_강민지.py
@@ -1,19 +1,28 @@
 """
 - 틀린 이유 : 연속된 부분 수열일 필요 X
 """
-
-from itertools import combinations
-
-n, S = map(int, input().split())
-seq = list(map(int, input().split()))
-answer = 0
-
-
-for i in range(1, n+1):
-    comb_list = combinations(seq, i)
+def recursive(idx, curr_sum, S, seq):
     
-    for c in comb_list:
-        if sum(c) == S:
-            answer += 1
+    global answer 
 
-print(answer)
+    if curr_sum == S: # 이게 종료 조건이 아님. curr_sum==S에서 부분수열이 늘어날 수 있음
+        answer += 1
+
+    if idx-1 == len(seq): # 주의! 종료 조건 필요
+        return
+    
+    for i in range(idx+1, len(seq)):
+        curr_sum += seq[i]
+        recursive(i, curr_sum, S, seq)
+        curr_sum -= seq[i]  # backtracking
+
+
+if __name__=="__main__":
+
+    n, S = map(int, input().split())
+    seq = list(map(int, input().split()))
+    answer = 0
+
+    for idx in range(n):
+        recursive(idx, seq[idx], S, seq)
+    print(answer)
