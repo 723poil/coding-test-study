7921f8437a69a170ca05b76e4c82c14a1fd7c779
aacara <budgielover@naver.com>
2024-05-03 12:48:26 +0900

    7주차_04_부분수열의_합_김현지

---
+++ week7/04.부분수열의 합/부분수열의_합_김현지.py
@@ -0,0 +1,25 @@
+def main(N, S, sequences):
+    visited = [False] * N
+    cnt = 0
+
+    def subSequence(idx, sum):
+        nonlocal cnt
+        # 종료 조건
+        if idx == N:
+            if sum == S:
+                cnt += 1
+            return
+        # 재귀 확장
+        subSequence(idx+1, sum+sequences[idx])
+        subSequence(idx+1, sum)
+
+    subSequence(0, 0)
+    if S == 0:
+        print(cnt-1)
+    else:
+        print(cnt)
+
+if __name__ == '__main__':
+    N, S = map(int, input().split())
+    sequences = list(map(int, input().split()))
+    main(N, S, sequences)
